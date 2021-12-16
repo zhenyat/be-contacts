d556ef6538225b92995a7c6dea9378d1f65d7b01
Zhenya Telyukov <telyukov@gmail.com>
2021-12-16 12:19:44 +0300

    admin initiated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,19 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+/***
+ *  v. 3.13:  Bootstrap 5 - jQuery + @popperjs/core
+ ***/
+ import Rails from "@rails/ujs"
+ import Turbolinks from "turbolinks"
+ import * as ActiveStorage from "@rails/activestorage"
+ import "channels"
+ 
+ Rails.start()
+ Turbolinks.start()
+ ActiveStorage.start()
+ 
+ import 'bootstrap'
+ import "@fortawesome/fontawesome-free/js/all";
+ // import "@fortawesome/fontawesome-free/css/all"
+ import '../stylesheets/application'
+ 
+require("trix")
+require("@rails/actiontext")
---
+++ app/models/concerns/emailable.rb
@@ -0,0 +1,13 @@
+################################################################################
+#   Handles and validates email attribute to be saved
+################################################################################
+module Emailable
+  extend ActiveSupport::Concern
+
+  included do
+    before_save {self.email.downcase!}
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  end
+end
